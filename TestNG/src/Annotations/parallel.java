b441197b6a417d910f61af11f839686c411f737b
LENOVO <LENOVO@LAPTOP-EMAAOB8C>
2019-03-11 13:09:36 +0530

    this is done for testing git with selenium project

---
+++ TestNG/src/Annotations/parallel.java
@@ -0,0 +1,33 @@
+package Annotations;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class parallel {
+	
+	@Test
+	public void getFirefox(){
+                //System.setProperty("webdriver.gecko.driver", "geckodriver.exe path");
+		System.setProperty("webdriver.gecko.driver", "D://Selenium Environment//Drivers//geckodriver.exe");
+                System.out.println("GetFirefox Method is running on Thread : " + Thread.currentThread().getId());
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://www.SoftwareTestingMaterial.com");
+		driver.close();
+	}
+	
+	@Test
+	public void getChorme(){
+                //System.setProperty("webdriver.chrome.driver", "chromedriver.exe path");
+		System.setProperty("webdriver.chrome.driver", "D://Selenium Environment//Drivers//chromedriver.exe");
+                System.out.println("GetChrome Method is running on Thread : " + Thread.currentThread().getId());
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.SoftwareTestingMaterial.com");
+		driver.close();
+	}
+
+}
+public class parallel {
+
+}
